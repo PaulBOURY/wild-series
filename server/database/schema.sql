81a406742e4ac160c81d7af4d575c0cbe8779c4a
PAUL BOURY <paulboury17@gmail.com>
2024-06-04 20:35:00 +0200

    Quest 5 Harmonia

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,20 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+CREATE TABLE category (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(100)
+);
+
+CREATE TABLE program (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  synopsis TEXT NOT NULL,
+  poster VARCHAR(255) NOT NULL,
+  coutry VARCHAR(100) NOT NULL,
+  year INT NOT NULL,
+  category_id INT NOT NULL,
+  FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
